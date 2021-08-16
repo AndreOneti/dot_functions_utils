0bdc394d45ad30d235f1e2e6d9926e8877744323
André Carvalho <40392351+AndreOneti@users.noreply.github.com>
2021-08-16 10:13:11 -0300

    :sparkles: String.map method
        [ADD] New mwthod .map from String - to map all letters

--- src/prototypes/String_prototype.ts
+++ src/prototypes/String_prototype.ts
@@ -11,38 +11,44 @@ interface String {
   toJson(): object | string;
   log(): string;
   log(msg: string): string;
+  map(
+    callbackfn: (value: string, index: number, array: string[]) => string
+  ): string;
 }
 
 String.prototype.twoDigits = function () {
-  if (0 <= Number(this) && Number(this) < 10) return "0" + Number(this).toString();
+  if (0 <= Number(this) && Number(this) < 10)
+    return "0" + Number(this).toString();
   return Number(this).toString();
 };
 
 String.prototype.threeDigits = function () {
-  if (0 <= Number(this) && Number(this) < 10) return "00" + Number(this).toString();
-  else if (10 <= Number(this) && Number(this) < 100) return "0" + Number(this).toString();
+  if (0 <= Number(this) && Number(this) < 10)
+    return "00" + Number(this).toString();
+  else if (10 <= Number(this) && Number(this) < 100)
+    return "0" + Number(this).toString();
   else return Number(this).toString();
 };
 
 String.prototype.captalize = function () {
   return this.substr(0, 1).toUpperCase() + this.substr(1).toLowerCase();
-}
+};
 
 String.prototype.captalizeAll = function () {
   let finalString = this.trim();
-  return finalString.replace(/\b[a-z]/g, char => char.toUpperCase());
-}
+  return finalString.replace(/\b[a-z]/g, (char) => char.toUpperCase());
+};
 
 String.prototype.splitToArray = function (minLength: number) {
-  let index = Math.floor((this.length / minLength));
+  let index = Math.floor(this.length / minLength);
   /* istanbul ignore next */
-  if ((this.length % minLength) > 0) index = Number(index) + 1;
+  if (this.length % minLength > 0) index = Number(index) + 1;
   let array: string[] = [];
   for (let i = 0; i < index; i++) {
     array.push(this.slice(minLength * i, minLength * (i + 1)));
   }
   return array;
-}
+};
 
 String.prototype.encodeBase64 = function () {
   try {
@@ -50,9 +56,9 @@ String.prototype.encodeBase64 = function () {
     return window.btoa(this.toString());
   } catch (error) {
     /* istanbul ignore next */
-    return Buffer.from(this).toString('base64');
+    return Buffer.from(this).toString("base64");
   }
-}
+};
 
 String.prototype.decodeBase64 = function () {
   try {
@@ -60,30 +66,43 @@ String.prototype.decodeBase64 = function () {
     return window.atob(this.toString());
   } catch (error) {
     /* istanbul ignore next */
-    return Buffer.from(this.toString(), 'base64').toString();
+    return Buffer.from(this.toString(), "base64").toString();
   }
-}
+};
 
 String.prototype.decapitalize = function () {
-  let [first, ...rest] = this.split('');
-  return first.toLowerCase() + rest.join('');
-}
+  let [first, ...rest] = this.split("");
+  return first.toLowerCase() + rest.join("");
+};
 
 String.prototype.reverse = function () {
   return this.split("").reverse().join("");
-}
+};
 
 String.prototype.toJson = function () {
   try {
-    return JSON.parse('' + this);
+    return JSON.parse("" + this);
   } catch (error) {
     return this;
   }
-}
+};
 
 String.prototype.log = function () {
   /* istanbul ignore next */
   let arg = arguments[0];
-  console.log("[ " + (new Date()).format('dd/mm/yyyy - hh:mm:ss') + " ] " + (arg ? ("(" + arg + ") ") : "") + "> ", "" + this);
+  console.log(
+    "[ " +
+      new Date().format("dd/mm/yyyy - hh:mm:ss") +
+      " ] " +
+      (arg ? "(" + arg + ") " : "") +
+      "> ",
+    "" + this
+  );
   return "" + this;
-}
+};
+
+String.prototype.map = function (
+  callbackfn: (value: string, index: number, array: string[]) => string
+): string {
+  return this.split("").map(callbackfn).join("");
+};
