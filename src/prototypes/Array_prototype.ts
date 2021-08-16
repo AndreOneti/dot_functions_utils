30d61638b67c663388ee4eb37ed6b297bfbc6b87
André Carvalho <40392351+AndreOneti@users.noreply.github.com>
2021-08-16 09:57:38 -0300

    :sparkles: Array.equal method
    [ADD] New mwthod .equal from array - to compare two arrays

--- src/prototypes/Array_prototype.ts
+++ src/prototypes/Array_prototype.ts
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 interface Array<T> {
   getLastElement(): T;
@@ -6,33 +6,31 @@ interface Array<T> {
   getMiddleElement(): Array<T> | null;
   log(): Array<T>;
   log(msg: string): Array<T>;
-  forEachSync(callback: (element: any, index: number, array: this) => Promise<any>): void;
+  forEachSync(
+    callback: (element: any, index: number, array: this) => Promise<any>
+  ): void;
+  equals(array: Array<T>): boolean;
 }
 
 Array.prototype.getLastElement = function () {
-  if (this.length <= 0)
-    return null;
+  if (this.length <= 0) return null;
   return this[this.length - 1];
 };
 
 Array.prototype.getFirstElement = function () {
-  if (this.length <= 0)
-    return null;
+  if (this.length <= 0) return null;
   return this[0];
 };
 
 Array.prototype.getMiddleElement = function () {
-  if (this.length <= 0)
-    return null;
+  if (this.length <= 0) return null;
   let middle = [Math.floor(this.length / 2)];
   let elements: any[] = [];
-  this.length % 2 == 0
-    ? middle.push(Math.floor(this.length / 2) - 1)
-    : null;
+  this.length % 2 == 0 ? middle.push(Math.floor(this.length / 2) - 1) : null;
   /* istanbul ignore next */
-  middle.sort((a, b) => (a > b) ? 1 : -1);
+  middle.sort((a, b) => (a > b ? 1 : -1));
 
-  middle.forEach(el => {
+  middle.forEach((el) => {
     elements.push(this[el]);
   });
   return elements;
@@ -41,12 +39,34 @@ Array.prototype.getMiddleElement = function () {
 Array.prototype.log = function () {
   /* istanbul ignore next */
   let arg = arguments[0];
-  console.log("[ " + (new Date()).format('dd/mm/yyyy - hh:mm:ss') + " ] " + (arg ? ("(" + arg + ") ") : "") + "> ", this);
+  console.log(
+    "[ " +
+      new Date().format("dd/mm/yyyy - hh:mm:ss") +
+      " ] " +
+      (arg ? "(" + arg + ") " : "") +
+      "> ",
+    this
+  );
   return this;
-}
+};
 
 Array.prototype.forEachSync = async function (callback) {
-  for (let index = 0; index < this.length; index++) {
-    await callback(this[index], index, this);
+  for await (const item of this) {
+    await callback(item, this.indexOf(item), this);
   }
-}
+};
+
+Array.prototype.equals = function (array) {
+  if (this.length !== array.length) return false;
+
+  for (var i = 0, l = this.length; i < l; i++) {
+    if (this[i] instanceof Array && array[i] instanceof Array) {
+      return this[i].equals(array[i]);
+    } else if (this[i] !== array[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+Object.defineProperty(Array.prototype, "equals", { enumerable: false });
